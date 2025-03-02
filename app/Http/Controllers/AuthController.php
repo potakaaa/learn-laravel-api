36a29de27ace18d0d06cb3054619f7438ce17359
potakaaa <helbirog@gmail.com>
2025-03-02 17:57:42 +0800

    register and login now working

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,13 +9,10 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+
+    // Sign up
     public function register(Request $request)
     {
-        // $validated = $request->validate([
-        //     'name' => 'required|string|max:200',
-        //     'email' => 'required|string|email|max:200|unique:users',
-        //     'password' => 'required|string|min:8',
-        // ]);
 
         $validated = Validator::make($request->all(), [
             'name' => 'required|string|max:200',
@@ -24,7 +21,7 @@ class AuthController extends Controller
         ]);
 
         if ($validated->fails()) {
-            return response()->json($validated->errors(), 400);
+            return response()->json($validated->errors(), 403);
         };
 
         try {
@@ -34,6 +31,38 @@ class AuthController extends Controller
                 'password' => Hash::make($request->password)
             ]);
 
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'access_token' => $token,
+                'user' => $user
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 403);
+        }
+    }
+
+
+    // Log in
+    public function login(Request $request)
+    {
+        $validated = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:6'
+        ]);
+
+        if ($validated->fails()) {
+            return response()->json($validated->errors(), 403);
+        }
+
+        $credentials = ['email' => $request->email, 'password' => $request->password];
+
+        try {
+            if (!auth()->attempt($credentials)) {
+                return response()->json(['error' => "Invalid credentials"], 400);
+            }
+
+            $user = User::where('email', $request->email)->firstorFail();
             $token = $user->createToken('auth_token')->plainTextToken;
 
             return response()->json([
